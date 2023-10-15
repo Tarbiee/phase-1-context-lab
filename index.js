edb2fb4bb926a1ab376566b84c19e211f32b6c9f
Tarbiee <dorcusstaby@gmail.com>
2023-10-15 22:16:00 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -9,7 +9,62 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+
+ function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+
+  function createEmployeeRecords(employeeArray) {
+    return employeeArray.map(createEmployeeRecord);
+  }
+  //createTimeInEvent
+  function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+        const timeInEvent = {
+      type: "TimeIn",
+      hour: parseInt(hour),
+      date,
+    };
+        this.timeInEvents.push(timeInEvent);
+        return this;
+  }
+
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+        const timeOutEvent = {
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date,
+    };
+        this.timeOutEvents.push(timeOutEvent);   
+    return this;
+  }
+
+  function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find((event) => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find((event) => event.date === date);
+    if (timeInEvent && timeOutEvent) {
+      const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+      return hoursWorked;
+    }
+      return 0;
+  }
+
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date); // Calculate hours worked using hoursWorkedOnDate
+      const payRate = this.payPerHour;
+      const wagesEarned = hoursWorked * payRate;
+    return wagesEarned;
+  }
+
+  const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -20,4 +75,25 @@ const allWagesFor = function () {
 
     return payable
 }
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function(employee) {
+      return employee.firstName === firstName;
+    });
+  }  
+
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce(function(totalPay, employee) {
+      return totalPay + allWagesFor.call(employee);
+    }, 0);
+  }
+  
+  
+  
+
+
+
+
+
+
 
